6145c56c6b22c425237e6b062575cac394ad20ab
Sai <github@saizai.com>
2019-03-10 14:27:13 +0000

    handle annoying filenames and cruft

--- rm_dupes_from.rb
+++ rm_dupes_from.rb
@@ -38,7 +38,8 @@ else
   new = ARGV[0].sub(/\/$/,'')
   orig = ARGV[1].sub(/\/$/,'')
   unless File.directory?(orig) && File.directory?(new)
-    puts "Arguments must be directories."
+    puts "#{orig} is not a directory." if !File.directory?(orig)
+    puts "#{new} is not a directory." if !File.directory?(new)
     exit
   end
 end
@@ -49,10 +50,10 @@ puts "#{orig_ff.size} files in #{orig}"
 new_ff = Dir.glob("#{new}/**/**").select{|f| File.file? f}
 puts "#{new_ff.size} files in #{new}"
 
-files_to_check = orig_ff.map{|f| f.sub(/^#{orig}\//,'')} & new_ff.map{|f| f.sub(/^#{new}\//,'')}
-ff_size = files_to_check.size
-puts "#{ff_size} files in both; deleting from #{new} if md5-equal to file in #{orig}"
-
+# files_to_check = orig_ff.map{|f| f.sub(/^#{Regexp.escape orig}\//,'')} & new_ff.map{|f| f.sub(/^#{Regexp.escape new}\//,'')}
+# ff_size = files_to_check.size
+# puts "#{ff_size} files in both; deleting from #{new} if md5-equal to file in #{orig}"
+ff_size = new_ff.size
 
 def size_scale s
   return s.inspect unless s.is_a? Integer
@@ -71,32 +72,57 @@ pruneable = {}
 size = 0
 i=0
 deleted = 0
-files_to_check.each do |f|
+# files_to_check.each do |f|
+new_ff.map{|f| f.sub(/^#{Regexp.escape new}\//,'')}.each do |f|
   i += 1
   of = File.join(orig, f)
   nf = File.join(new, f)
-  if File.file?(of) && File.file?(nf)
-    begin
-      fs = File.size(nf)
-      if (fs == File.size(of)) && (Digest::MD5.file(of).to_s == Digest::MD5.file(nf).to_s)
-        pruneable[File.dirname(f)] = '?'
-        File.delete(nf)
-        size += fs
-        deleted += 1
-        puts "#{i}\t#{"%d%%" % (100.0*i/ff_size)}\t=D\t#{size_scale fs}\t#{size_scale size}\t#{f}"
-      else
-        puts "#{i}\t#{"%d%%" % (100.0*i/ff_size)}\t≠\t#{size_scale fs}\t#{size_scale size}\t#{f}"
-      end
-    rescue => e
-      puts e.full_message
-      puts "#{i}\t#{"%d%%" % (100.0*i/ff_size)}\t=!\t\t#{size_scale size}\t#{f}"
-    end
+  if prune && f =~ /(Icon.?\r?|\.DS_Store|desktop\.ini)$/im
+    pruneable[File.dirname(f)] = '?'
+    puts "\t\tx-\t\t#{size_scale size}\t#{f.inspect}"
   else
-    puts "#{i}\t#{"%d%%" % (100.0*i/ff_size)}\t !\t\t#{size_scale size}\t#{f}"
-  end
+    if File.file?(nf)
+      fs = File.size(nf)
+      if File.file?(of)
+        begin
+            print "#{i}\t#{"%d%%" % (100.0*i/ff_size)}\t"
+          if (fs == File.size(of)) && (Digest::MD5.file(of).to_s == Digest::MD5.file(nf).to_s)
+            print "="
+            pruneable[File.dirname(f)] = '?'
+            File.delete(nf)
+            size += fs
+            deleted += 1
+            puts "D\t#{size_scale fs}\t#{size_scale size}\t#{f.inspect}"
+          else
+            puts "≠\t#{size_scale fs}\t#{size_scale size}\t#{f.inspect}"
+          end
+        rescue => e
+          puts "!\t\t#{size_scale size}\t#{f.inspect}"
+          puts e.full_message
+        end
+      else # in new but not in old
+        begin
+          print ">"
+          pruneable[File.dirname(f)] = '?'
+          FileUtils.mkdir_p (File.dirname of)
+          File.rename(nf, of)
+          # size += fs
+          deleted += 1
+          puts "D\t#{size_scale fs}\t#{size_scale size}\t#{f.inspect}"
+        rescue => e
+          puts "!\t\t#{size_scale size}\t#{f.inspect}"
+          puts e.full_message
+        end
+      end # file? of
+    else
+      puts "nf not a file #{nf}"
+    end # file? nf
+  end # prune
 end
 
 if prune
+  pruneable[''] = '?'
+  pruneable.delete('.')
   while !pruneable.keys.empty?
     dd = pruneable.keys.sort.reverse
     dd_size = dd.size
@@ -105,16 +131,35 @@ if prune
     i=0
     dd.each do |d|
       i+=1
-      nd = File.join(new, d)
-      if File.directory?(nd) && Dir.empty?(nd)
-        begin
-          parent = File.dirname(nd)
-          Dir.rmdir(nd)
-          pruneable[parent] = '?'
-          puts "#{i}\t#{"%d%%" % (100.0*i/dd_size)}\t/D\t\t\t#{nd}"
-        rescue => e
-          puts e.full_message
-          puts "#{i}\t#{"%d%%" % (100.0*i/dd_size)}\t/!\t\t\t#{nd}"
+      nd = (d == '' ? new : File.join(new, d))
+      if File.directory?(nd)
+        Dir.glob("#{nd}/*").each do |f|
+          if f =~ /(Icon.?\r?|\.DS_Store|desktop\.ini)$/im
+            begin
+              print "\t\tx"
+              pruneable[File.dirname(f)] = '?'
+              pruneable[''] = '?'
+              File.delete(f)
+              deleted += 1
+              puts "D\t\t\t#{f.inspect}"
+            rescue => e
+              puts "!\t\t\t#{f.inspect}"
+              puts e.full_message
+            end
+          end
+        end
+        if Dir.empty?(nd)
+          begin
+            print "#{i}\t#{"%d%%" % (100.0*i/dd_size)}\t/"
+            parent = File.dirname(nd)
+            Dir.rmdir(nd)
+            pruneable[parent] = '?'
+            pruneable[''] = '?'
+            puts "D\t\t\t#{nd.inspect}"
+          rescue => e
+            puts "!\t\t\t#{nd.inspect}"
+            puts e.full_message
+          end
         end
       end
     end
