2ba2c77f77c49df555311f476882bc6c944f9910
Sai <github@saizai.com>
2019-07-25 23:34:00 +0100

    update

--- bulk_rn.sh
+++ bulk_rn.sh
@@ -5,9 +5,9 @@ require 'fileutils'
 pre = Regexp.new(ARGV[0])
 post = ARGV[1]
 
-puts "pre\t#{pre.inspect}"
-puts "post\t#{post.inspect}"
-puts "pwd\t#{Dir.pwd.inspect}"
+File.open('bulk_rn.log','a') do |f|
+f.sync = true
+f << "\n\nTime.now.to_s\npre\t#{pre.inspect}\npost\t#{post.inspect}\npwd\t#{Dir.pwd.inspect}"
 
 [true,false].each do |d|
 
@@ -21,20 +21,28 @@ end.sort.reverse
 to_rename.map do |xx|
   newname = xx.gsub(pre,post)
   FileUtils.mkdir_p File.dirname(newname)
-  if !File.exist?(newname)
-    FileUtils.mv(xx, newname)
-    print "Moved"
-  elsif !File.directory?(newname) && FileUtils.compare_file(xx, newname)
-    FileUtils.mv(xx, newname)
-    print "Moved (identical)"
-  elsif File.directory?(newname)
-    print "Directory exists - noop"
-  else
-    print "Files differ - not moving"
+  begin
+    msg = ""
+    if !File.exist?(newname)
+      FileUtils.mv(xx, newname)
+      msg = "Moved"
+    elsif !File.directory?(newname) && FileUtils.compare_file(xx, newname)
+      FileUtils.mv(xx, newname)
+      msg = "Moved (identical)"
+    elsif File.directory?(newname)
+      msg = "Directory exists - noop"
+    else
+      msg = "Files differ - not moving"
+    end
+    f << "\n#{msg}\t#{xx.inspect}\t#{newname.inspect}"
+  rescue => e
+    f << "\nerror\t#{xx.inspect}\t#{newname.inspect}\n"
+    f << e.message
   end
-  puts "\t#{xx.inspect}\t#{newname.inspect}"
 end
 
-puts "Done! #{to_rename.size} #{d ? "directories" : "files"} renamed."
+f << "Done! #{to_rename.size} #{d ? "directories" : "files"} renamed."
+
+end
 
 end
